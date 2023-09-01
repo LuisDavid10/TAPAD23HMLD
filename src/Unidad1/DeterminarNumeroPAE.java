c6c57e1a3db5a1280d478d0aed951467b535eb5e
LuisDavid10 <l22050369@saltillo.tecnm.mx>
2023-09-01 14:06:13 -0600

    Programa de DeterminarNumeroPAE

--- src/Unidad1/DeterminarNumeroPAE.java
+++ src/Unidad1/DeterminarNumeroPAE.java
@@ -7,21 +7,38 @@ public class DeterminarNumeroPAE {
 	public static void main(String[] args) {
 		int num = Integer.parseInt(JOptionPane.showInputDialog("Determine un número"));
 		//Determinar si es primo 
-		
-		//no es primo
-		int contador = 1;
-		for (int i = 2; i <= num / 2; i++) {
-			if (num % i == 0) {
-				contador = contador + i;
-			}
-		}
-		if (num == contador) {
-			JOptionPane.showConfirmDialog(null, "El numero  " + num + " es perfecto");
-		} else if (num < contador) {
-			JOptionPane.showConfirmDialog(null, "El numero " + num + " es abundante con " + contador);
-		} else {
-			JOptionPane.showConfirmDialog(null, "El numero " + num + " es escaso con " + contador);
-		}
+        if (num <= 1) {
+            JOptionPane.showMessageDialog(null, "El número no es primo");
+        } else {
+            boolean esPrimo = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+
+            if (esPrimo) {
+                JOptionPane.showMessageDialog(null, "El número es primo");
+            } else {
+                JOptionPane.showMessageDialog(null, "El número no es primo");
+              //no es primo
+        		int contador = 1;
+        		for (int i = 2; i <= num / 2; i++) {
+        			if (num % i == 0) {
+        				contador = contador + i;
+        			}
+        		}
+        		if (num == contador) {
+        			JOptionPane.showConfirmDialog(null, "El numero  " + num + " es perfecto");
+        		} else if (num < contador) {
+        			JOptionPane.showConfirmDialog(null, "El numero " + num + " es abundante con " + contador);
+        		} else {
+        			JOptionPane.showConfirmDialog(null, "El numero " + num + " es escaso con " + contador);
+        		}
+            }
+        }
+   		
 	}
 
-}
+	}
--- src/Unidad1/DeterminarPrimo.java
+++ src/Unidad1/DeterminarPrimo.java
@@ -15,11 +15,11 @@ public class DeterminarPrimo {
 		}else if (num==4) {
 			JOptionPane.showMessageDialog(null, "El número no es primo");
 		}else {
-			int mitad=(num/2)-1;
+			int mitad=(num/2);
 			boolean respuesta=false;
 		for(int i=1;i<=mitad;i++) {
 			int contador=2;
-			if(contador/i==0) {
+			if(contador%i==0) {
 			respuesta=false;
 			}else {
 			respuesta = true;
@@ -33,62 +33,84 @@ public class DeterminarPrimo {
 			JOptionPane.showMessageDialog(null, "El número no es primo");
 		}
 		}
-
-		//opcion del profe
-		system.out.println("dame un valor entero positivo");
-numero=entrada.nextInt();
-//usando contador 
-int cantidadDivisores;
-//se inicializa el controlador en cero
-//en este caso siempre hay 2 divisores
-//la unidad y el mismo valor
-cantidadDivisores=2;
-//usando variable de estado(switch,bandera)
-for(int j=2;1<= numero / 2;j++){
-	//dependiendo de una condicion
-	if(numero%1==0){
-		//se incrementa el contador
-		cantidadDivisores++;
-		//se modifica el estado de la variable switch o bandera 
 	}
 }
-//se verifica el valor o estado de la variable 
-if(cantidadDivisores>0){
-	//se determina si es primo
-	System.out.println("Es primo");
-}else{
-	System.out.println("No es primo");
+
+        if (num <= 1) {
+            JOptionPane.showMessageDialog(null, "El número no es primo");
+        } else {
+            boolean esPrimo = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+
+            if (esPrimo) {
+                JOptionPane.showMessageDialog(null, "El número es primo");
+            } else {
+                JOptionPane.showMessageDialog(null, "El número no es primo");
+            }
+        }
+    }
 }
 
-
-//usando variable de estado(switch, bandera)
-//cuando son 2 posibilidades se declara de tipo boolean
-//si mas de 2 lo mas comun es que sean enteros o char (no float)
-boolean isPrimo;
-//se inicializa en lo opuesto dependiendo de lo que se puede verificar
-//que cambie el estado ( todo numero es verdadero a menos de que demuestres lo contrario)
-isPrimo=true;
-
-for(int j=2;1<= numero / 2;j++){
-	//dependiendo de una condicion
-	if(numero%1==0){
-		//se incrementa el contador
-		cantidadDivisores++;
-		//se modifica el estado de la variable switch o bandera 
-		isPrimo=false;
-	}
-
-//se verifica el valor o estado de la variable 
-//ya sea un conteo
+//		//opcion del profe
+//		system.out.println("dame un valor entero positivo");
+//numero=entrada.nextInt();
+////usando contador 
+//int cantidadDivisores;
+////se inicializa el controlador en cero
+////en este caso siempre hay 2 divisores
+////la unidad y el mismo valor
+//cantidadDivisores=2;
+////usando variable de estado(switch,bandera)
+//for(int j=2;1<= numero / 2;j++){
+//	//dependiendo de una condicion
+//	if(numero%1==0){
+//		//se incrementa el contador
+//		cantidadDivisores++;
+//		//se modifica el estado de la variable switch o bandera 
+//	}
+//}
+////se verifica el valor o estado de la variable 
 //if(cantidadDivisores>0){
-	//o de estado
-if(isPrimo){
-	//se determina si es primo
-	System.out.println("Es primo");
-}else{
-	System.out.println("No es primo");
-}
-	}
-}
-}
-
+//	//se determina si es primo
+//	System.out.println("Es primo");
+//}else{
+//	System.out.println("No es primo");
+//}
+//
+//
+////usando variable de estado(switch, bandera)
+////cuando son 2 posibilidades se declara de tipo boolean
+////si mas de 2 lo mas comun es que sean enteros o char (no float)
+//boolean isPrimo;
+////se inicializa en lo opuesto dependiendo de lo que se puede verificar
+////que cambie el estado ( todo numero es verdadero a menos de que demuestres lo contrario)
+//isPrimo=true;
+//
+//for(int j=2;1<= numero / 2;j++){
+//	//dependiendo de una condicion
+//	if(numero%1==0){
+//		//se incrementa el contador
+//		cantidadDivisores++;
+//		//se modifica el estado de la variable switch o bandera 
+//		isPrimo=false;
+//	}
+//
+////se verifica el valor o estado de la variable 
+////ya sea un conteo
+////if(cantidadDivisores>0){
+//	//o de estado
+//if(isPrimo){
+//	//se determina si es primo
+//	System.out.println("Es primo");
+//}else{
+//	System.out.println("No es primo");
+//}
+//	}
+//}
+//}
+//
