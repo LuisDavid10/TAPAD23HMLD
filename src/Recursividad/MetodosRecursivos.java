75324a9d321978f800ecbfbac8eb88265152e83a
LuisDavid10 <L22050369@saltillo.tecnm.mx>
2023-09-14 09:05:32 -0600

    Cuenta Progresiva y Regresiva de manera Recursiva 14/09/23

--- src/Recursividad/MetodosRecursivos.java
+++ src/Recursividad/MetodosRecursivos.java
@@ -30,4 +30,31 @@ public class MetodosRecursivos {
 			return numero * factorialRecursivo(numero - 1);
 		}
 	}
+	private int  Suma1n (int n) trows Exception {
+		if(n>= 1) {
+			return 
+		}
+		return n + Suma1n (n -1 );
+	}
+	
+	private int cuentaRegresivaRecursiva (int n) {
+		if (n == 1) { //true
+			//caso base
+			System.out.println("En: "+ 1);
+		}else if (n>1) {
+			//caso recursivo
+			System.out.println("En: "+ n);
+			cuentaRegresivaRecursiva ( n-1 );
+	}
+		public void cuentaProgresivaRecursiva(int n) {
+			if(n==1) {
+				//caso base
+				System.out.println(1);
+			}else {
+				//caso recursivo
+				cuentaProgresivaRecursiva (n-1);
+				System.out.println(n);
+				}
+		}
+
 }
--- src/Recursividad/Suma1n.java
+++
@@ -1,5 +0,0 @@
-package Recursividad;
-
-public int  Suma1n (int n) {
-	return n + Suma1n (n -1 );
-}
