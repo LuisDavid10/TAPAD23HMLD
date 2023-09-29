95d0ce1f744aa03d1fc2b20e050934fc88d187a1
LuisDavid10 <L22050369@saltillo.tecnm.mx>
2023-09-29 09:06:35 -0600

    Decimal a binario y cuenta de division Recursiva 29/09/23

--- src/Recursividad/MetodosRecursivos.java
+++ src/Recursividad/MetodosRecursivos.java
@@ -1,15 +1,17 @@
 package Recursividad;
 
+import javax.swing.JOptionPane;
+
 public class MetodosRecursivos {
 
 	/**
 	 * metodo jefe 
 	 */
-	public int factorial (int numero) {
-		if(numero >=0) {
-			return factorialRecursivo(numero);
-		}
-	}
+	// public int factorial (int numero) {
+	// 	if(numero >=0) {
+	// 		return factorialRecursivo(numero);
+	// 	}
+	// }
 	/**
 	 * metodo trabajador
 	 * @param numero
@@ -30,11 +32,11 @@ public class MetodosRecursivos {
 			return numero * factorialRecursivo(numero - 1);
 		}
 	}
-	private int  Suma1n (int n) trows Exception {
-		if(n>= 1) {
-			return 
+	private int  Suma1nRecursivo (int n)  {
+		if(n == 1) {
+			return 1; 
 		}
-		return n + Suma1n (n -1 );
+		return n + Suma1nRecursivo (n -1 );
 	}
 	
 	private int cuentaRegresivaRecursiva (int n) {
@@ -45,6 +47,7 @@ public class MetodosRecursivos {
 			//caso recursivo
 			System.out.println("En: "+ n);
 			cuentaRegresivaRecursiva ( n-1 );
+		}
 	}
 		public void cuentaProgresivaRecursiva(int n) {
 			if(n==1) {
@@ -61,46 +64,101 @@ public class MetodosRecursivos {
 		/**
 		 * 
 		 */
-		private void MaximoComunDivisor (int M, int N) {
-			if(N>M) {
-				int a;
-				a=N;
-				N=M;
-				M=a;
-			}else {
-				//caso base
-				if(N=0) {
-					MaximoComunDivisor = M;
-				}else{
-					MaximoComunDivisor = MaximoComunDivisor (N, M % N );
-				}
-			}
-		}
+		// private void MaximoComunDivisor (int M, int N) {
+		// 	if(N>M) {
+		// 		int a;
+		// 		a=N;
+		// 		N=M;
+		// 		M=a;
+		// 	}else {
+		// 		//caso base
+		// 		if(N=0) {
+		// 			MaximoComunDivisor = M;
+		// 		}else{
+		// 			MaximoComunDivisor = MaximoComunDivisor (N, M % N );
+		// 		}
+		// 	}
+		// }
 
 		/**
 		 * 
 		 */
-		public static void main (String[] args){
-			Scanner = new Scanner.in (System.in);
+		// public static void main (String[] args){
+		// 	Scanner = new Scanner.in (System.in);
 
-			if(M > 0 && N>=0){
-				MaximoComunDivisor(M, N);
-			}else{
-				Exception e;
-			}
-		}
+		// 	if(M > 0 && N>=0){
+		// 		MaximoComunDivisor(M, N);
+		// 	}else{
+		// 		Exception e;
+		// 	}
+		// }
 
 		/**
 		 * Metodo recursivo para calcular X^y
 		 * donde x es la base y y es el exponente 
 		 */
-		public int potenciaRecursivo(int x, int y){
-			if(y == 0){
-				return x;
-			}else if(y==1){
-				return x;
-			}else{
-				return x(NumeroExponencial (x,y-1));
+		// public int potenciaRecursivo(int x, int y){
+		// 	if(y == 0){
+		// 		return x;
+		// 	}else if(y==1){
+		// 		return x;
+		// 	}else{
+		// 		return x(NumeroExponencial (x,y-1))*x;
+		// 	}
+		// }
+		
+		/**
+		 * An example if a method - replace this comment with your own 
+		 * 
+		 * @param y a sample parameter for a metod
+		 * @return the sum of x and y
+		 */
+		private  int residuoRecursivo(int dividendo, int divisor) {
+			//caso base, ya tengo resultado 
+			if(dividendo < divisor) {
+				return dividendo;
+			}else {
+				return residuoRecursivo(dividendo - divisor, divisor); 
 			}
+		}
+		/**
+		 * An example if a method - replace this comment with your own 
+		 * 
+		 * @param y a sample parameter for a method
+		 * @return the sum of x and y
+		 */
+		public  int residuo(int dividendo, int divisor) throws Exception{
+			if (dividendo > 0 && divisor > 0) {
+				return residuoRecursivo(dividendo, divisor);
+			}else {
+				throw new Exception("Los valores deben ser positivos");
+			}
+		}
+		//sacar el valor de el contador 
+		private int cocienteRecursivo(int dividendo, int divisor){
+			//caso base
+			if(dividendo<divisor){
+				return 0;
+			}else{
+				return cocienteRecursivo(dividendo - divisor, divisor) +1;
+			}
+
+		}
+
+		/**
+		 * 
+		 */
+		public String decimalABinario(int decimal){
+			if(decimal <= 1){
+				//regresar cadena con el valor decimal
+				(decimal);
+						
+			}else{
+				decimalABinario( decimal / 2);
+				//el resultado (residuo se agrega a la cadena que recibo)
+				(decimal % 2);
+			}
+
+			//terminar el programa 
 		}
 }
